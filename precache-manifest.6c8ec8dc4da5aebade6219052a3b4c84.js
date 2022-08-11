899cca8d7dc3bb879bfa8f4dc3608c729b4aec5b
Uday Singh <110370486+Matrixbravo@users.noreply.github.com>
2022-08-11 19:34:52 +0530

    Updates

---
+++ precache-manifest.6c8ec8dc4da5aebade6219052a3b4c84.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "15177e17455ece63c84a3eb0e7e8a26d",
+    "url": "/searchDatabase/index.html"
+  },
+  {
+    "revision": "38b6d1a288f8fc34f5aa",
+    "url": "/searchDatabase/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "209ed343a6c2f99c6dd0",
+    "url": "/searchDatabase/static/js/2.08431e12.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/searchDatabase/static/js/2.08431e12.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "38b6d1a288f8fc34f5aa",
+    "url": "/searchDatabase/static/js/main.7c2e5812.chunk.js"
+  },
+  {
+    "revision": "4e0e34f265fae8f33b01b27ae29d9d6f",
+    "url": "/searchDatabase/static/js/main.7c2e5812.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "febd746b42ffa146b4fc",
+    "url": "/searchDatabase/static/js/runtime-main.832f0bb0.js"
+  }
+]);
